4a7361a30e2089378bb67d1dbee63489fa620511
DartPower <SIMPSON-NEKEK@mail.ru>
2019-08-18 19:32:22 +0300

    Try to add locale system

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Globalization;
 
 namespace MBGE
 {
@@ -7,6 +9,20 @@ namespace MBGE
         static void Main(string[] args)
         {
             Console.WriteLine("[" + typeof(System.DateTime) + "] " + "Ok!");
+            __MBGE_PressEnter();
+        }
+
+        private static void __MBGE_PressEnter()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("ru-RU");
+            Console.WriteLine(Properties.locale.PressEnter);
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
         }
     }
 }
