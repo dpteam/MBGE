9e96e26e00981b768ad1df27d122d5ba9486ef9b
DartPower <SIMPSON-NEKEK@mail.ru>
2019-08-18 19:49:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Threading;
 using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace MBGE
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("[" + typeof(System.DateTime) + "] " + "Ok!");
-            __MBGE_PressEnter();
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Marshal.PrelinkAll(typeof(Program));
+			__MBGE_LogInit();
+			Trace.WriteLine("[" + DateTime.UtcNow.ToString() + "] " + "Ok!");
+			__MBGE_PressEnter();
+		}
 
-        private static void __MBGE_PressEnter()
-        {
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("ru-RU");
-            Console.WriteLine(Properties.locale.PressEnter);
-            do
-            {
-                while (!Console.KeyAvailable)
-                {
-                    // Do something
-                }
-            } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
-        }
-    }
+		private static void __MBGE_LogInit()
+		{
+			Trace.AutoFlush = true;
+			Trace.Listeners.Clear();
+			Trace.Listeners.Add(new ConsoleTraceListener());
+			Trace.Listeners.Add(new TextWriterTraceListener("MBGE.log"));
+		}
+
+		private static void __MBGE_PressEnter()
+		{
+			Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("ru-RU");
+			Trace.WriteLine(Properties.locale.PressEnter);
+			do
+			{
+				while (!Console.KeyAvailable) { }
+			} while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+		}
+	}
 }
